ff5af3b2a16b5a62965c61760911df5d6d6e30d4
aniketgope00 <aniketgope00@gmail.com>
2025-04-07 01:47:30 +0530

    adding the models

---
+++ models/load_models.py
@@ -0,0 +1,13 @@
+import ultralytics
+
+def install_models()->None:
+    pass
+
+def get_model()->list:
+    pass
+
+
+
+if __name__ == "__main__":
+    install_models()
+    print("Models installed successfully.")
